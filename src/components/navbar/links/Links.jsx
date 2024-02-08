b757b7af639863738f554ad3840ac305f5006e4d
Alexey Efimik <a.efimik@gmail.com>
2024-02-08 22:40:23 +0400

    Refactor navbar links component and add temporary session and isAdmin variables

--- src/components/navbar/links/Links.jsx
+++ src/components/navbar/links/Links.jsx
@@ -1,35 +1,32 @@
-import Link from "next/link";
 import styles from "./links.module.css";
 import NavLink from "./navLink/NavLink";
 
 const Links = () => {
-
     const links = [
-        {
-        title: "Homepage",
-        path: "/"
-        },
-        {
-        title: "About",
-        path: "/about"
-        },
-        {
-        title: "Contact",
-        path: "/contact"
-        },
-        {
-        title: "Blog",
-        path: "/blog"
-        }
+        { title: "Homepage", path: "/" },
+        { title: "About", path: "/about" },
+        { title: "Contact", path: "/contact" },
+        { title: "Blog", path: "/blog" }
     ];
-    
+
+    // TEMPORARY: Hardcoded values for session and isAdmin
+    const session = true;
+    const isAdmin = true;
+
     return (
         <div className={styles.links}>
-          {links.map((link) => (
-            <NavLink item={link} key={link.title} />
-          ))}
+            {links.map((link) => (
+                <NavLink item={link} key={link.title} />
+            ))}
+            {session && (
+                <>
+                    {isAdmin && <NavLink item={{ title: "Admin", path: "/admin" }} />}
+                    <button className={styles.logout}>Logout</button>
+                </>
+            )}
+            {!session && <NavLink item={{ title: "Login", path: "/login" }} />}
         </div>
     );
 };
-    
+
 export default Links;
